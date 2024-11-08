90a5bc6e1299ce9f3f77f2dc9ccf44a30d4898dc
Charleston Campos <charlinhocjc@gmail.com>
2024-11-08 16:27:44 -0300

    Vídeo 3.2 - Notificações no navegador

--- src/app/shared/components/timer-control/timer-control.component.ts
+++ src/app/shared/components/timer-control/timer-control.component.ts
@@ -1,9 +1,10 @@
 import { Howl } from 'howler';
 import { CommonModule } from '@angular/common';
-import { Component, effect, Inject, inject } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { ContextService, ContextType } from '../../services/context.service';
 import { FormsModule } from '@angular/forms';
 import { AudioService } from '../../services/audio.service';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-timer-control',
@@ -32,7 +33,9 @@ export class TimerControlComponent {
 
   context = this.contextService.contextSignal$;
 
-  constructor() {
+  constructor(
+    private notificationService: NotificationService
+  ) {
     effect(() => {
       this.setTimerSecond();
       this.configTimer();
@@ -76,6 +79,8 @@ export class TimerControlComponent {
       this.setTimerSecond();
       this.configTimer();
 
+      this.sendNotification();
+
       return;
     }
 
@@ -104,6 +109,28 @@ export class TimerControlComponent {
       case 'descanso-longo':
         this.timerInSeconds = 15;
         break;
+    }
+  }
+
+  private async sendNotification() {
+    try {
+      await this.notificationService.requestPermission();
+
+      const context = this.context();
+
+      if (context.includes('descanso')) {
+        this.notificationService.showNotification('Notificação', {
+          body: 'Tempo de descanso finalizado!',
+        });
+
+        return;
+      }
+
+      this.notificationService.showNotification('Notificação', {
+        body: 'Tempo de foco finalizado!',
+      });
+    } catch (error) {
+      console.error('Erro ao enviar notificação:', error);
     }
   }
 }
